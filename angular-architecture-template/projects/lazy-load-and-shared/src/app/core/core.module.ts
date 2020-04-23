0942c34824adc78e90543c894fff7a0f5374dd6e
skrikl <sergey.kriklivyy@gmail.com>
2020-04-24 00:57:41 +0300

    fix: core-module: updated imports

--- angular-architecture-template/projects/lazy-load-and-shared/src/app/core/core.module.ts
+++ angular-architecture-template/projects/lazy-load-and-shared/src/app/core/core.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    // vendor
+    BrowserModule,
+    BrowserAnimationsModule
+
+  ],
+  exports: []
 })
 export class CoreModule { }
