2fe696956a350a4256a04faf6991565846d24238
skrikl <sergey.kriklivyy@gmail.com>
2020-04-25 14:16:15 +0300

    new: core-module: added MarModules for the Toolbar

--- angular-architecture-template/projects/lazy-load-and-shared/src/app/core/core.module.ts
+++ angular-architecture-template/projects/lazy-load-and-shared/src/app/core/core.module.ts
@@ -2,15 +2,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+// material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
+
 // components
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
-
 @NgModule({
     declarations: [MainLayoutComponent],
     imports: [
         // vendor
         BrowserModule,
         BrowserAnimationsModule,
+        RouterModule,
+        // material
+        MatToolbarModule,
+        MatButtonModule
     ],
     exports: [MainLayoutComponent],
 })
